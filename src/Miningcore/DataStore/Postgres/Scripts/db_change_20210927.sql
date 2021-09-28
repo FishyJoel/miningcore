44694e4c50148edd65e2f29ff2e3a55ac4def622
Joseph Burley <joseph.burley@nortonlifelock.com>
2021-09-28 18:15:01 -0400

    Put primary key in other update script

--- src/Miningcore/DataStore/Postgres/Scripts/db_change_20210927.sql
+++ src/Miningcore/DataStore/Postgres/Scripts/db_change_20210927.sql
@@ -3,3 +3,5 @@ ADD COLUMN accepted TIMESTAMP NOT NULL;
 
 CREATE INDEX IDX_SHARES_POOL_ACCEPTED ON shares(poolid, accepted);
 
+ALTER TABLE shares 
+ADD PRIMARY KEY (poolid, miner, accepted);
--- src/Miningcore/DataStore/Postgres/Scripts/db_change_20210928.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE shares 
-ADD PRIMARY KEY IF NOT EXISTS (poolid, miner, accepted);
